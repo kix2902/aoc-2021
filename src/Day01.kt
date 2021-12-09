9a76d01fc95ccaddc67feb14a09efe52ae52579b
Ismael Reyes <ismael.kix2902@gmail.com>
2021-12-09 18:25:20 +0100

    Day01-part2 completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -4,14 +4,21 @@ fun main() {
         input.forEachIndexed { i, num ->
             when {
                 i == 0 -> total = 0
-                num.toInt() > input[i-1].toInt() -> total++
+                num.toInt() > input[i - 1].toInt() -> total++
             }
         }
         return total
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var total = 0
+        val nums = input.map { it.toInt() }
+        for (index in 3 until nums.size) {
+            val old = nums[index - 3] + nums[index - 2] + nums[index - 1]
+            val new = nums[index - 2] + nums[index - 1] + nums[index]
+            if (new > old) total++
+        }
+        return total
     }
 
     // test if implementation meets criteria from the description, like:
