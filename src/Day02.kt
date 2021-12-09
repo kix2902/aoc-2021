f92b7a7e69334036e7902a865bbef68a4d7c80c9
Ismael Reyes <ismael.kix2902@gmail.com>
2021-12-09 18:55:44 +0100

    Day02-part2 completed

--- src/Day02.kt
+++ src/Day02.kt
@@ -15,10 +15,33 @@ fun main() {
         return pos * depth
     }
 
+    fun part2(input: List<String>): Int {
+        var pos = 0
+        var depth = 0
+        var aim = 0
+
+        input.forEach {
+            var parts = it.split(" ")
+            when (parts[0]) {
+                "down" -> aim += parts[1].toInt()
+                "up" -> aim -= parts[1].toInt()
+                "forward" -> {
+                    val value = parts[1].toInt()
+                    pos += value
+                    depth += aim * value
+                }
+            }
+        }
+
+        return pos * depth
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
 
     val input = readInput("Day02")
     println(part1(input))
+    println(part2(input))
 }
