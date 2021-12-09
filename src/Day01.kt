8c9e162042ab91790f5bf97bfd724309ac702109
Ismael Reyes <ismael.kix2902@gmail.com>
2021-12-09 18:15:19 +0100

    Day01-part1 completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,13 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var total = 0
+        input.forEachIndexed { i, num ->
+            when {
+                i == 0 -> total = 0
+                num.toInt() > input[i-1].toInt() -> total++
+            }
+        }
+        return total
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +16,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
