0fa20f7a8193083af45d2ba97b33a1acf8c08c88
Ismael Reyes <ismael.kix2902@gmail.com>
2021-12-10 20:23:20 +0100

    Day03-part2 completed

--- src/Day03.kt
+++ src/Day03.kt
@@ -19,10 +19,40 @@ fun main() {
         return (num * inverse).toInt()
     }
 
+    fun part2(input: List<String>): Int {
+        var oxygen = input.toList()
+        var co2 = input.toList()
+
+        var index = 0
+        while ((oxygen.size > 1) || (co2.size > 1)) {
+            if (oxygen.size > 1) {
+                val oxygenMapCount = oxygen.map { it[index] }.groupingBy { it }.eachCount()
+                val maxValue = oxygenMapCount.values.maxOrNull()
+                val maxEntries = oxygenMapCount.filterValues { it == maxValue }
+                val selected = if (maxEntries.size > 1) '1' else maxEntries.keys.first()
+                oxygen = oxygen.filter { it[index] == selected }
+            }
+            if (co2.size > 1) {
+                val co2MapCount = co2.map { it[index] }.groupingBy { it }.eachCount()
+                val minValue = co2MapCount.values.minOrNull()
+                val minEntries = co2MapCount.filterValues { it == minValue }
+                val selected = if (minEntries.size > 1) '0' else minEntries.keys.first()
+                co2 = co2.filter { it[index] == selected }
+            }
+            index++
+        }
+
+        val oxygenNum = Integer.parseInt(oxygen.first(), 2)
+        val co2Num = Integer.parseInt(co2.first(), 2)
+        return oxygenNum * co2Num
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 198)
+    check(part2(testInput) == 230)
 
     val input = readInput("Day03")
     println(part1(input))
+    println(part2(input))
 }
