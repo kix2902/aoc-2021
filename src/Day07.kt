ecfa246275ba929ad774f69a20ecbfe2be031963
Ismael Reyes <ismael.kix2902@gmail.com>
2021-12-12 19:46:48 +0100

    Day07-part2 completed

--- src/Day07.kt
+++ src/Day07.kt
@@ -8,16 +8,29 @@ fun main() {
             it[it.size / 2]
     }
 
+    fun seriesSum(n: Int) = n * (n + 1) / 2
+
     fun part1(input: List<String>): Int {
         val nums = input[0].split(",").map { it.toInt() }
         val avg = med(nums)
-        return nums.map { (it - avg).absoluteValue }.sum()
+        return nums.sumOf { (it - avg).absoluteValue }
+    }
+
+    fun part2(input: List<String>): Int {
+        val nums = input[0].split(",").map { it.toInt() }
+        val maxPos = nums.maxOrNull()!!
+        val minPos = nums.minOrNull()!!
+        return (minPos..maxPos).map { pos ->
+            nums.sumOf { num -> seriesSum((num - pos).absoluteValue) }
+        }.minOrNull()!!
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
     check(part1(testInput) == 37)
+    check(part2(testInput) == 168)
 
     val input = readInput("Day07")
     println(part1(input))
+    println(part2(input))
 }
