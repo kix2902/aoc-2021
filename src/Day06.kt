c4147a6d192b5847ad07166236a3b9e3df51acc5
Ismael Reyes <ismael.kix2902@gmail.com>
2021-12-12 20:43:00 +0100

    Day06-part2 completed

--- src/Day06.kt
+++ src/Day06.kt
@@ -19,10 +19,34 @@ fun main() {
         return fishes.size
     }
 
+    fun part2(input: List<String>): Long {
+        val fishes = input[0].split(",").map { it.toInt() }
+        val fishesMap = fishes.groupingBy { it }.eachCount().mapValues { it.value.toLong() }.toMutableMap()
+
+        repeat(256) {
+            var temp: Long = 0
+            (0..9).forEach { timer ->
+                temp = fishesMap.getOrDefault(timer, 0)
+                fishesMap[timer] = 0
+                fishesMap[timer - 1] = temp
+            }
+            if (fishesMap.getOrDefault(-1, 0) > 0) {
+                val dead = fishesMap.getOrDefault(-1, 0)
+                fishesMap[6] = fishesMap.getOrDefault(6, 0) + dead
+                fishesMap[8] = dead
+            }
+        }
+
+        fishesMap[-1] = 0
+        return fishesMap.values.sum()
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
     check(part1(testInput) == 5934)
+    check(part2(testInput) == 26984457539)
 
     val input = readInput("Day06")
     println(part1(input))
+    println(part2(input))
 }
